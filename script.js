8cd148543ba0963ead9f6ff387bd2a7f8b0c81d4
jollycoder <dfiveg@gmail.com>
2016-08-21 19:13:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener('load', function () {
     animateButton({
         elem: document.getElementsByClassName('button_inversion')[0],
         fillToCursor: true,                             // если true, заливка при наведении будет идти к курсору, если false — к центру
-        fillScreenOnClick: false,                        // если true, при клике по кнопке заливаем весь экран
+        fillScreenOnClick: true,                        // если true, при клике по кнопке заливаем весь экран
         clickScreenFillColor: 'rgba(22, 77, 139, .5)',  // цвет заливки экрана при клике
         clickButtonFillColor: '#164d8b',                // цвет заливки кнопки при клике
         clickTextColor: 'white',                        // цвет текста кнопки при клике
